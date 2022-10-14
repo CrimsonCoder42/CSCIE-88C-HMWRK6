2f1ebd06a7183264bfee53159c7de1422f76d34e
CrimsonCoder42 <nostro37@gmail.com>
2022-10-14 07:56:55 -0400

    week 6 finish

--- src/test/scala/org/cscie88c/week6/AddableTypeclassTest.scala
+++ src/test/scala/org/cscie88c/week6/AddableTypeclassTest.scala
@@ -7,12 +7,18 @@ class AddableTypeclassTest extends StandardTest {
   "AddableAggregator" should {
     "sum a list of integers" in {
       // add your unit tests here
+      val intListTest = List(1,2,3,4,5,6,7)
+      AddableAggregator.sumWithAddable(intListTest).equals(28)
     }
     "sum a list of booleans" in {
       // add your unit tests here
+      val boolListTest = List(false, true, false, true)
+      AddableAggregator.sumWithAddable(boolListTest) shouldBe(true)
+
     }
     "sum a list of employees" in {
       // add your unit tests here
+
     }
   }
 }
--- src/test/scala/org/cscie88c/week6/EmployeeTest.scala
+++ src/test/scala/org/cscie88c/week6/EmployeeTest.scala
@@ -7,10 +7,27 @@ class EmployeeTest extends StandardTest {
  
     "have a default sort order" in {
       // write unit tests here
+      val e1 = Employee("Zaphod",33,178852)
+      val e2 = Employee("Ford",37,189359)
+      val e3 = Employee("Arthur",43,170273)
+      val e4 = Employee("Marvin",23,267907)
+      val empList = List(e1, e2, e3, e4)
+
+      val testVal = empList.sorted
+      testVal.head.name should equal("Arthur")
+
     }
 
     "sort employees by salary" in {
+
+      val e1 = Employee("Zaphod", 43, 178852)
+      val e2 = Employee("Ford", 27, 189359)
+      val e3 = Employee("Arthur", 33, 170273)
+      val e4 = Employee("Marvin", 48, 267907)
+      val testList = List(e1, e2, e3, e4)
       // write unit tests here
+      val empSalSort = Employee.sortEmployeesBySalary(testList)
+      empSalSort.head.salary should equal(267907)
     }
   }
 }
